8f66ec8b0051a0c33b73783bacc562fad6945e2d
Facundo <fnicolasquiroga@gmail.com>
2026-03-01 23:29:36 -0300

    continuando unidad 5

---
+++ unidad5/ejercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+class Program
+{
+     static void Main(string[] args)
+    {
+       // 2. Hacer un programa que solicite el ingreso de 10 números y que muestre el mayor de ellos por pantalla. 
+        //Solo se debe emitir UN valor por pantalla.
+         int  n = 0, mayor =0;
+
+        for (int x = 0; x < 10; x++)
+        {
+            Console.WriteLine("ingrese un nuemero");
+            n = int.Parse(Console.ReadLine()!);
+
+        
+        }
+        mayor = mayor > n;
+          Console.WriteLine("el mayor es :" + n) ;
+
+
+        
+    } 
+
+    
+}
+    
